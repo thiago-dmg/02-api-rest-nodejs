0e02faba5c87ba5854011910e04d2d222d8eeedc
Thiago <bussoladev@gmail.com>
2024-02-08 20:03:36 -0300

    feat(*): Configuração de ambientes com Zod

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,29 @@
 import fastify from "fastify";
 import crypto from "node:crypto";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const transaction = await knex("transactions")
-    .insert({
-      id: crypto.randomUUID(),
-      title: "Transação de Teste",
-      amount: 1000,
-    })
-    .returning("*");
+  // const transaction = await knex("transactions")
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: "Transação de Teste",
+  //     amount: 1000,
+  //   })
+  //   .returning("*");
 
-  return transaction;
+  const transactios = await knex("transactions")
+    .where("amount", 1000)
+    .select("*");
+
+  return transactios;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server Running!");
