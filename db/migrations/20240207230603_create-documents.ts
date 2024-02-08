ae86a1a3dab6d8a57ed41d9bc828f0ea282b8117
Thiago <bussoladev@gmail.com>
2024-02-08 18:54:45 -0300

    feat(*): Resolve field amount in Knex

--- db/migrations/20240207230603_create-documents.ts
+++ db/migrations/20240207230603_create-documents.ts
@@ -1,5 +1,13 @@
-import type { Knex } from "knex";
+import { Knex } from "knex";
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("transactions", (table) => {
+    table.uuid("id").primary();
+    table.text("title").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now()).notNullable();
+  });
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("transactions");
+}
