0024368ca4b4c96e9879c6ce80a86ed43cad2c63
Thiago <bussoladev@gmail.com>
2024-02-08 22:55:23 -0300

    feat(*): Criando Transações

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,12 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
+// import crypto from "node:crypto";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  // const transaction = await knex("transactions")
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: "Transação de Teste",
-  //     amount: 1000,
-  //   })
-  //   .returning("*");
-
-  const transactios = await knex("transactions")
-    .where("amount", 1000)
-    .select("*");
-
-  return transactios;
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app
